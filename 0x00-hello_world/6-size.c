5239870cd0ccb9c29c1ea3be3e2e8645bf1f3c43
Bello Ibrahim <wasiubello60@gmail.com>
2023-02-16 12:09:14 -0800

    Correct task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * 
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/* Variable definition and initializing*/
-	int a = puts("Size of a char: %lu byte(s)", (unsigned long)sizeof(d));
-	long int b = puts("Size of an int: %lu byte(s)", (unsigned long)sizeof(a));
-	long long int c = puts("Size of a long int: %lu byte(s)", (unsigned long)sizeof(b));
-	char d = puts("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	float f = puts("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf(d);
-	printf(a);
-	printf(b);
-	printf(c);
-	printf(f);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
