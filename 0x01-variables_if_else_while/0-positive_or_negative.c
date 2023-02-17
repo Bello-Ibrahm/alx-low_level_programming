8199130f871dcaf6e78d236a7ec47d774d9b4155
Bello Ibrahim <wasiubello60@gmail.com>
2023-02-17 08:41:42 -0800

    Correct the betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * Function main - Determines  if a number is positive, negative or zero
+ * main - Determines if a number is positive, negative or zero.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
