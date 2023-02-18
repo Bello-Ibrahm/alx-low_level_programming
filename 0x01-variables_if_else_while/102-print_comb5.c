10e7d14e781ca86d96b265eeb09e291fc2989206
Bello Ibrahim <wasiubello60@gmail.com>
2023-02-18 05:48:34 -0800

    Add task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int w, x, y, z, opt1, opt2;
+
+	w = x = y = z = 48;
+	while (z < 58)
+	{
+		y = 48;
+		while (y < 58)
+		{
+			x = 48;
+			while (x < 58)
+				w = 48;
+			while (w < 58)
+			{
+				opt1 = (z * 10) + y;
+				opt2 = (x * 10) + w;
+				if (opt1 < opt2)
+				{
+					putchar(z);
+					putchar(y);
+					putchar(' ');
+					putchar(x);
+					putchar(w);
+					if (z == 57 && y == 56 && x == 57 && w == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				w++;
+			}
+			x++;
+		}
+		y++;
+	}
+	z++;
+}
+putchar('\n');
+return (0);
+}
