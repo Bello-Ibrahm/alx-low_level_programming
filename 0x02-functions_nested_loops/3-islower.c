68a72ae2eb75bd72ee4e4565084d7b5048e145da
Bello Ibrahim <wasiubello60@gmail.com>
2023-02-21 11:25:46 -0800

    Add task 3 islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/** 
+ * islower - main function
+ *
+ * @c: Charaacter to compare
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
