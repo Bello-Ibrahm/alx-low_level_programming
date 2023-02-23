aecb68dbd2c8944e4201e408ababb0942d56818f
Bello Ibrahim <wasiubello60@gmail.com>
2023-02-23 08:39:23 -0800

    Initial commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
