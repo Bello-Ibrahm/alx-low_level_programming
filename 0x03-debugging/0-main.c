b4f873dff49359a2c776fbffe6f9a7dc56c908f8
Bello Ibrahim <wasiubello60@gmail.com>
2023-02-23 08:59:13 -0800

    Correct task 0 and 2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * main - Determines if a number is positive, negative or zero.
- * 
- * Return: Always 0 (Success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
