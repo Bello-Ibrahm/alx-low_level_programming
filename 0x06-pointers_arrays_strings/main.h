a765c2fde9f527d934248297bd9957ad41e7a2c7
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-02 04:03:24 -0800

    Add task 8-9

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,4 +17,18 @@ char *cap_string(char *);
 
 char *leet(char *);
 
+char *rot13(char *);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
+/**
+ * _putchar - Writes the character c to std
+ * @c: Parameter to compare
+ */
+int _putchar(char c);
+
 #endif
