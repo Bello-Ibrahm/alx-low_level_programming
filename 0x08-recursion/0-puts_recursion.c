ad226576f170f7e4c271ef4b974fbfb0da580dc0
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-08 08:07:25 -0800

    Fix betty error

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * _puts_recursion - uses recursion
- * @s: string to recurse through
- * 
+ * _puts_recursion - Uses recursion
+ * @s: String to recurse through
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s)
