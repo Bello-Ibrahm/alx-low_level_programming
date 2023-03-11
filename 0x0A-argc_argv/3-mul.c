b5fcb95907227f8b9b10199e62fabcf46d05bb31
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-11 06:23:07 -0800

    Fix betty error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,9 +4,9 @@
 /**
  * main - Write a program that multiplies two numbers.
  * 
- * Your program should print the result of the multiplication, followed by a new line
- * You can assume that the two numbers and result of the multiplication can be stored in an integer
- * If the program does not receive two arguments, your program should print Error, followed by a new line, and return 1
+ * Your program should print the result of the multiplication, followed by a new * line
+ * You can assume that the two numbers and result of the multiplication can be s * tored in an integer
+ * If the program does not receive two arguments, your program should print Erro * r, followed by a new line, and return 1
  *
  * @argc: This is the argument count
  * @argv: This is the argument vector
