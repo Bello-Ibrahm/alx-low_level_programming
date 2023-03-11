b6d31477fda9d0e403240091e9b4dfea24138452
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-11 08:11:39 -0800

    Fix error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <ctype.h>
 /**
  * main - Write a program that adds positive numbers.
  *
@@ -14,32 +14,33 @@
  *
  * Return: This return to 1 if error else 0
  */
-int main(int argc, char* argv[])
+
+int main(int argc, char *argv[])
 {
-	int i, sum;
-       
-	sum = 0;
+	int sum = 0, i;
 
 	if (argc > 1)
 	{
-
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] >= 'a' && *argv[i] <= 'z')
+			int b;
+			char *str;
+
+			str = argv[i];
+			for (b = 0; str[b] != '\0'; b++)
 			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				sum += atoi(argv[i]);
+				if (str[b] < 48 || str[b] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
 		}
-		printf("%d\n", sum);
-		return (0);
 	}
-	else
+	for (i = 1; i < argc; i++)
 	{
-		printf("%d\n", 0);
+		sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
+	return (0);
 }
