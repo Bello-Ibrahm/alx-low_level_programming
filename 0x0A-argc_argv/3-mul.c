d89e1d871c4ae1e90b0cd321383b7cbfb8342ce6
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-11 06:42:51 -0800

    Fix coding style

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,18 +18,15 @@ int main(int argc, char* argv[])
 	int i, multiplication;
        
 	multiplication = 1;
-	if (argc > 2)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			multiplication *= atoi(argv[i]);
-		}
-		printf("%d\n", multiplication);
-		return (0);
-	}
-	else
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		multiplication *= atoi(argv[i]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
 }
