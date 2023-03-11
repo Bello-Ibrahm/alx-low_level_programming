963be6f31007f3b2a8dc30e3e34a8916aad547c5
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-11 02:33:45 -0800

    Fix error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - Prints the name of itself
- * @argc: argumentc number of the command line arguments
- * @argv[]: vector of arguments, an array of size argc
+ * @argc: argument count - number of the command line arguments
+ * @argv: argument vector - an array of size argc
+ * 
  * Return: Always 0 (Success)
  */
 
