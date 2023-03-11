f5098e9eacd1f2d6a6f854454c91e24c93f47c48
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-11 06:45:11 -0800

    Fix coding style

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,16 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - Write a program that multiplies two numbers.
- * 
- * Your program should print the result of the multiplication, followed by a new * line
- * You can assume that the two numbers and result of the multiplication can be s * tored in an integer
- * If the program does not receive two arguments, your program should print Erro * r, followed by a new line, and return 1
- *
+ * main - Program that multiplies two numbers.
  * @argc: This is the argument count
  * @argv: This is the argument vector
  *
- * Return: This return to 1 if error else 0
+ * Return: 0;
  */
 int main(int argc, char* argv[])
 {
