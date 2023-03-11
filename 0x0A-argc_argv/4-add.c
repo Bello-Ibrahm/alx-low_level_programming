dda2dbc2f390d3bbc70418ae6a5eaac28c3e5e1b
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-11 08:14:22 -0800

    Fix coding style

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,20 +1,15 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
 /**
- * main - Write a program that adds positive numbers.
- *
- * Print the result, followed by a new line
- * If no number is passed to the program, print 0, followed by a new line
- * If one of the number contains symbols that are not digits, print Error, follo * wed by a new line, and return 1
- * You can assume that numbers and the addition of all the numbers can be stored * in an int
- *
+ * main - Program that adds positive numbers
  * @argc: This is the argument count
  * @argv: This is the argument vector
  *
- * Return: This return to 1 if error else 0
+ * Return: 0;
  */
-
 int main(int argc, char *argv[])
 {
 	int sum = 0, i;
