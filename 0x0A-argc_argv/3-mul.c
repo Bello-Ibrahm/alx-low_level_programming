54e8ffecf20d830c280e55b4b5b3a70545db92c3
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-11 08:17:48 -0800

    Fix error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,35 +1,26 @@
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - Write a program that multiplies two numbers.
- * 
- * Your program should print the result of the multiplication, followed by a new * line
- * You can assume that the two numbers and result of the multiplication can be s * tored in an integer
- * If the program does not receive two arguments, your program should print Erro * r, followed by a new line, and return 1
- *
+ * main - Program that multiplies two numbers.
  * @argc: This is the argument count
  * @argv: This is the argument vector
  *
- * Return: This return to 1 if error else 0
+ * Return: 0;
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int i, multiplication;
-       
+	int index, multiplication;
+
 	multiplication = 1;
-	if (argc > 2)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			multiplication *= atoi(argv[i]);
-		}
-		printf("%d\n", multiplication);
-		return (0);
-	}
-	else
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
 }
