96b1e65a2745442e202dbbd5f3bbddf28ac8696a
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-14 12:26:30 -0700

    Fix task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,44 +8,43 @@
  * @argv: Vector of arguments
  * Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char  *argv[])
 {
-        int money, coins = 0;
+	int money, coins = 0;
 
-        if (argc == 2)
-        {
-                if (strchr(argv[argc - 1], '-'))
-                {
-                        printf("%d\n", 0);
-                        return (1);
-                }
+	if (argc == 2)
+	{
+		if (strchr(argv[argc - 1], '-'))
+		{
+			printf("0\n");
+			return (1);
+		}
+		
+		money = atoi(argv[argc - 1]);
 
-                money = atoi(argv[argc - 1]);
-
-                while (money > 0)
-                {
-                        if (money % 25 == 0)
-                        {
-                                money -= 25;
-                        } else if (money % 10 == 0)
-                        {
-                                money -= 10;
-                        } else if (money % 5 == 0)
-                        {
-                                money -= 5;
-                        } else if (money % 2 == 0)
-                        {
-                                money -= 2;
-                        } else
-                        {
-                                money--;
-                        }
-                        coins++;
-                }
-                printf("%d\n", coins);
-                return (0);
-        }
-        printf("%s\n", "Error");
-       return (1);
+		while (money > 0)
+		{
+			if (money % 25 == 0)
+			{
+				money -= 25;
+			} else if (money % 10 == 0)
+			{
+				money -= 10;
+			} else if (money % 5 == 0)
+			{
+				money -= 5;
+			} else if (money % 2 == 0)
+			{
+				money -= 2;
+			} else
+			{
+				money--;
+			}
+			coins++;
+		}
+		printf("%d\n", coins);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
 }
-
