c821e5902119d84f4a317a888c016bdc37f5670e
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-14 12:30:36 -0700

    Fix error

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,50 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 /**
  * main - Prints coins left
  * @argc: Parameter counter
  * @argv: Vector of arguments
  * Return: always 0
  */
-int main(int argc, char  *argv[])
+int main(int args, char *argv[])
 {
-	int money, coins = 0;
+	int  c, coins = 0;
 
-	if (argc == 2)
+	if (args != 2)
 	{
-		if (strchr(argv[argc - 1], '-'))
-		{
-			printf("0\n");
-			return (1);
-		}
-		
-		money = atoi(argv[argc - 1]);
-
-		while (money > 0)
-		{
-			if (money % 25 == 0)
-			{
-				money -= 25;
-			} else if (money % 10 == 0)
-			{
-				money -= 10;
-			} else if (money % 5 == 0)
-			{
-				money -= 5;
-			} else if (money % 2 == 0)
-			{
-				money -= 2;
-			} else
-			{
-				money--;
-			}
-			coins++;
-		}
-		printf("%d\n", coins);
+		printf("Error\n");
+		return (1);
+	}
+	c = atoi(argv[1]);
+	if (c < 0)
+	{
+		printf("0\n");
 		return (0);
 	}
-	printf("Error\n");
-	return (1);
+	for (; c >= 0;)
+	{
+		if (c >= 25)
+			c -= 25;
+
+		else if (c >= 10)
+			c -= 10;
+
+		else if (c >= 5)
+			c -= 5;
+
+		else if (c >= 2)
+			c -= 2;
+
+		else if (c >= 1)
+			c -= 1;
+		else
+			break;
+		coins += 1;
+	}
+	printf("%d\n", coins);
+	return (0);
 }
