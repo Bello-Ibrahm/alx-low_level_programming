d13bfdee072a237e5fbc7f582d910151dc236b07
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-22 09:13:22 -0700

    Add task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Iterates through array
+ *
+ * @array: Pointer to array to go through
+ * @size: Size of the array
+ * @action: Pointer action function to take
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if ((array == NULL) || (action == NULL))
+		exit(98);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
