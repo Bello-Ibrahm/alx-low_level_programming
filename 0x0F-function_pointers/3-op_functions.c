bdc05d887aa9b24d354a2f794765e604660bde81
Bello Ibrahim <wasiubello60@gmail.com>
2023-03-22 14:02:33 -0700

    Add task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-cal.h"
+
+/**
+ * op_add - Add functions
+ * @a: int a
+ * @b: int b
+ * Return: Sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts function
+ * @a: int a
+ * @b: int b
+ * Return: Difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplication function
+ * @a: int a
+ * @b: int b
+ * Return: Multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Division function
+ * @a: int a
+ * @b: int b
+ * Return: Division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulos funtion
+ * @a: int a
+ * @b: int b
+ * Return: Modulos of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
