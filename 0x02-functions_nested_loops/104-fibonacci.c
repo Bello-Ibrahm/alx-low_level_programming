6d34f4fa290fe00a7ed3434bd45307059f2764da
an-nur-info-tech <wasiubello60@gmail.com>
2023-07-19 10:05:50 -0700

    Correct task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 /**
- * main - main function
+ * main - Entry point
  *
- * Return: nothing
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int counter = 2;
+	unsigned long int a, b, c, d, m, n, z;
+	unsigned long int split1, split2, digit, thousand, cycl, limit;
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
-
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
+	m = 1;
+	n = 2;
+	limit = 96;
+	digit = 1000;
+	printf("%lu, ", m);
+	printf("%lu, ", n);
+	for (cycl = 1; cycl <= 90; cycl++)
 	{
-		counter++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (counter < 98)
-		{
-			printf(", ");
-		}
+		z = m + n;
+		m = n;
+		n = z;
+		printf("%lu, ", z);
 	}
-	printf("\n");
+	a = m / digit, b = m % digit;
+	c = n / digit, d = n % digit;
+	for (cycl = 91; cycl <= limit; cycl++)
+	{
+		thousand = (b + d) / digit;
+		split1 = (a + c) + thousand;
+		split2 = (b + d) - thousand * digit;
+		a = c, b = d;
+		c = split1, d = split2;
+		if (split2 >= 100)
+			printf("%lu%lu", split1, split2);
+		else
+			printf("%lu0%lu", split1, split2);
+		if (cycl != limit)
+			printf(", ");
+	}
+	putchar('\n');
 	return (0);
 }
