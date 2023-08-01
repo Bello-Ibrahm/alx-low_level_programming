3b1bc6934346dfcdbd0cb940135a4b6622ae7160
an-nur-info-tech <wasiubello60@gmail.com>
2023-08-01 03:10:47 -0700

    Fix task 9 #1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,21 @@
 #include "lists.h"
+
+/**
+ * listint_len -  get the number of elements in a linked listint_t list
+ * @h: head pointer of singly linked list
+ *
+ * Return: Number of elements in list
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int size = 0;
+
+	while (h != 0)
+		h = h->next, size++;
+
+	return (size);
+}
+
 /**
  * insert_nodeint_at_index - inserts node at index
  * @head: linked list
@@ -8,36 +25,47 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-	listint_t *tmp, *new;
+	unsigned int len = listint_len(*head), count = 0;
+	listint_t *new_node, *temp1, *temp2;
 
-	if (head == NULL)
-		return (NULL);
+	if (head == 0 && idx > 0)
+		return (0);
 
-	tmp = *head;
-	while (tmp != NULL && count != idx - 1)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == 0)
+		return (0);
+
+	if (head == 0 && idx == 0)
 	{
-		tmp = tmp->next;
-		count++;
+		new_node->n = n; 
+		new_node->next = 0;
+		*head = new_node;
+		return (new_node);
 	}
 
-	if (count != idx - 1 && idx != 0)
-		return (NULL);
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (idx != 0)
+	if (idx > len)
 	{
-		new->next = tmp->next;
-		tmp->next = new;
+		free(new_node);
+		return (0);
+	}
+	temp1 = *head;
+	new_node->n = n;
+	while (count < idx && idx != 0)
+	{
+		temp2 = temp1;
+		temp1 = temp1->next;
+		count++;
+	}
+	if (idx == 0)
+	{
+		*head = new_node;
+		new_node->next = temp1;
 	}
 	else
 	{
-		new->next = *head;
-		*head = new;
+		temp2->next = new_node;
+		new_node->next = temp1;
 	}
-	return (new);
+	return (new_node);
 }
 
