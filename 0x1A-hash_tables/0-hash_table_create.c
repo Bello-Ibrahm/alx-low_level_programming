bdf96c1dd026e616de54822b3174bc10c26a397e
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-10 08:38:53 -0400

    Fix checker

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t *hash_table_created;
+	unsigned long int i;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (!hash_table)
+	/* allocate space for the hash table */
+	hash_table_created = malloc(sizeof(hash_table_t));
+
+	if (hash_table_created == NULL)
 		return (NULL);
 
-	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
-	if (!(hash_table->array))
+	hash_table_created->size = size;
+	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (hash_table_created->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
 	{
-		free(hash_table);
-		return (NULL);
+		hash_table_created->array[i] = NULL;
 	}
 
-	return (hash_table);
+	return (hash_table_created);
 }
